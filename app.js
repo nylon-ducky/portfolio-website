4f32f9f6f91093bf96242b86fecc0f0fd8a35b01
Durso <dursozd@gmail.com>
2021-06-27 15:33:54 -0400

    progress

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+
+function post() {
     
+    let posts = [];
+
+    let guestBook = document.getElementById('guestBook');
+    let postForm = document.getElementById('postForm');
+    
+    
+    let name = document.getElementById('name').value;
+    let message = document.getElementById('message').value;
+    
+    
+
+}
+
+
+
