0c2a74fb54ef7ebca0a9466a76944b31667eda1e
Durso <dursozd@gmail.com>
2021-06-30 15:38:15 -0400

    ...

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 
+let posts = [];
+
 function post() {
     
-    let posts = [];
 
-    let guestBook = document.getElementById('guestBook');
-    let postForm = document.getElementById('postForm');
-    
-    
+   //document.getElementById('guestBook').innerHTML = document.getElementById('name').value;
+     
+    let gb = document.getElementById('guestBook');
+   
     let name = document.getElementById('name').value;
     let message = document.getElementById('message').value;
     
+    let nm = name + message;
+    posts.push(nm);
     
-
+    gb.innerHTML = posts;
+    /**/
+   
 }
 
 
